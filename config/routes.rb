4f32ca12cee4a27219638999ce98678d8008abd4
Surbhi <surbhib@crowdstaffing.com>
2023-04-05 20:54:21 +0530

    Add MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'login/index'
+  get '/', to: 'main#index' 
+  get 'history/index'
+  get 'about/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
