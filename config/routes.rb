a1469387ffba54a315b2f265c08f7f792168d465
Surbhi <surbhib@crowdstaffing.com>
2023-04-09 20:16:37 +0530

    Add login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
   get 'login/index'
