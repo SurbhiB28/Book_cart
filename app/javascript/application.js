3bc473bb1fc122dff3edafcbcb2a9a57afa84ae3
SurbhiB28 <surbhib@crowdstaffing.com>
2023-04-23 14:19:47 +0530

    Active storage for image uploads

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,4 +3,5 @@ import "@hotwired/turbo-rails"
 import "controllers"
 
 //= require jquery
-//= require bootstrap-sprockets
+//= require bootstrap-sprocketsimport "trix"
+import "@rails/actiontext"
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
     belongs_to :user
     belongs_to :category
+    has_one_attached :book_img
 end
