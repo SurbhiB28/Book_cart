9517cc0d1bee2c9e9434f5a29d72dfbc3b8b553d
SurbhiB28 <surbhib@crowdstaffing.com>
2023-04-24 15:40:56 +0530

    Add average rating

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,6 +9,11 @@ class BooksController < ApplicationController
         end
     end
     def show
+        if @book.reviews.blank?
+            @average_review = 0
+        else
+            @average_review = @book.reviews.average(:rating).round(2)
+        end
     end
     def edit
         @categories = Category.all.map{ |c| [c.name, c.id]}
